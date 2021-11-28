49307604fb0cbd0f67b79e7baf7d95437569a8b4
ericpandrade <ericpandrade085@gmail.com>
2021-11-28 19:42:36 -0300

    feat: adding productpage

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
+import 'package:fast_deal/view/loginpage.dart';
 import 'package:fast_deal/view/productpage.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: ProductPage(),
+    home: LoginPage(),
     debugShowCheckedModeBanner: false,
   ));
 }
