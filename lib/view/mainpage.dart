0cb4cb7ca7f94eff66f334815fd578b023c1d2df
otavioalbucosta <otavio-albucosta@hotmail.com>
2021-11-28 16:06:29 -0300

    navbar criada

--- lib/view/mainpage.dart
+++ lib/view/mainpage.dart
@@ -11,7 +11,21 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.cyan, title: Icon(Icons.check)),
+      appBar: AppBar(
+        backgroundColor: Colors.cyan,
+        title: Icon(Icons.check),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Chat"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Favoritos"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Você")
+        ],
+      ),
     );
   }
 }
