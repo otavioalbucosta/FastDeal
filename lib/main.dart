e0a5a0c5150326c41a62a444bff1dd4a84414a3b
Rone <rone.cslima@gmail.com>
2021-11-28 21:12:02 -0300

    announcement_page done

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
+import 'package:fast_deal/view/announcement_page.dart';
 import 'package:fast_deal/view/loginpage.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: LoginPage(),
+    home: AnnouncementPage(),
     debugShowCheckedModeBanner: false,
   ));
 }
